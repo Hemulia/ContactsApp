49ffb6b862af4a232a38dc72dd11de3494ed46d7
Emilia Mäkelä <bgr887@myy.haaga-helia.fi>
2023-03-13 01:17:26 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
+import * as Contacts from 'expo-contacts';
 
 export default function App() {
+  const [contacts, setContacts] = useState([]);
+
+  const getContacts = async () => {
+    const { status } = await Contacts.requestPermissionsAsync();
+    if (status === 'granted') {
+      const { data } = await Contacts.getContactsAsync({
+        fields: [Contacts.Fields.PhoneNumbers, Contacts.Fields.Name],
+      });
+      if (data.length > 0) {
+        setContacts(data);
+      }
+    }
+  };
+
+  const renderItem = ({ item }) => {
+    return (
+      <TouchableOpacity style={styles.itemContainer}>
+        <Text style={styles.itemText}>{item.name} - {item.phoneNumbers[0]?.number}</Text>
+      </TouchableOpacity>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{fontSize:30, marginBottom:80}}>Contacts</Text>
+      <TouchableOpacity style={styles.button} onPress={getContacts}>
+        <Text style={styles.buttonText}>Get Contacts</Text>
+      </TouchableOpacity>
+      <FlatList
+        data={contacts}
+        keyExtractor={(item) => item.id}
+        renderItem={renderItem}
+        style={styles.list}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#fff',
+    marginTop: 200
+  },
+  button: {
+    backgroundColor: '#1e88e5',
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  list: {
+    width: '100%',
+    paddingHorizontal: 10,
+  },
+  itemContainer: {
+    backgroundColor: '#eee',
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  itemText: {
+    fontSize: 16,
   },
 });
